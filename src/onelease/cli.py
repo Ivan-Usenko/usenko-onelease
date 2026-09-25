97619c46cc997914ed9deb34f32b4178d2781839
Usenko Ivan <iusenko272@gmail.com>
2026-09-25 11:30:19 +0000

    feat: use price parse in main function

--- src/onelease/cli.py
+++ src/onelease/cli.py
@@ -1,5 +1,9 @@
+from onelease.domain.parse import parse_price
+
+
 def main() -> None:
     print("OneLease 0.1.0 - середовище налаштовано")
+    print(f"Результат парсингу ціни \"1500 грн/міс\": {parse_price("1500 грн/міс")}")
 
 
 if __name__ == "__main__":
